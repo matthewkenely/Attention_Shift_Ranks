edb4017758261529cefcde8a1802c575f61e35e1
heliaea <lunasci@protonmail.com>
2023-08-10 13:01:15 +0200

    Removed keepdims arguments

--- Attention_Shift_Saliency_Rank/obj_sal_seg_branch/SOSNet.py
+++ Attention_Shift_Saliency_Rank/obj_sal_seg_branch/SOSNet.py
@@ -204,7 +204,7 @@ class SOSNet():
             layer = self.keras_model.get_layer(name)
             if layer.output in self.keras_model.losses:
                 continue
-            loss = (tf.reduce_mean(layer.output, keepdims=True)
+            loss = (tf.reduce_mean(layer.output)
                     * self.config.LOSS_WEIGHTS.get(name, 1.))
             self.keras_model.add_loss(loss)
 
@@ -227,7 +227,7 @@ class SOSNet():
                 continue
             layer = self.keras_model.get_layer(name)
             self.keras_model.metrics_names.append(name)
-            loss = (tf.reduce_mean(layer.output, keepdims=True)
+            loss = (tf.reduce_mean(layer.output)
                     * self.config.LOSS_WEIGHTS.get(name, 1.))
             self.keras_model.metrics_tensors.append(loss)
 
